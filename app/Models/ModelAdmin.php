1b89b8114990a7ffacaa3d8e5f8ca7e6c48b381d
raafzh <rafly.fajhary245.rf@gmail.com>
2023-06-18 19:16:27 +0700

    fix login
    memperbaiki halaman login

--- app/Models/ModelAdmin.php
+++ app/Models/ModelAdmin.php
@@ -6,55 +6,13 @@ use CodeIgniter\Model;
 
 class ModelAdmin extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'admin';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = ['username', 'password', 'telepon', 'alamat', 'email'];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
-
+    protected $table = 'admin';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['username', 'password', 'telepon', 'alamat', 'email'];
+    
     public function getUserByUsername($username)
-    {
-        return $this->where('username', $username)->first();
-    }
+{
+    return $this->where('username', $username)->first();
+}
 
-    public function getUserByPassword($password)
-    {
-        return $this->where('password', $password)->second();
-    }
-    // protected function hashPassword(array $data)
-    // {
-    //     if (isset($data['data']['password'])) {
-    //         $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
-    //     }
-
-    //     return $data;
-    // }
 }
